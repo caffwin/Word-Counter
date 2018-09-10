13fa55d2ae4a816b75e04af87cd73a8517b2e73b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 12:56:21 -0700

    Edited format, take sys.argv as argument

--- wordcount.py
+++ wordcount.py
@@ -1,21 +1,32 @@
 # put your code here.
-file_name = open("test.txt")
+# text_file = open("test.txt")
 
+import sys
 def words_count(file_name):
 
     counts = {}
 
-    for line in file_name:
-        print(line)
-        line = line.rstrip()
-        print(line)
-        words = line.split()
+    with open(file_name) as file1:
+        for line in file1:
+            # print(line)
+            # line = line.rstrip()
+            # print(line)
+            words = line.split()
 
-        for word in words:
-            word = word.rstrip(",")
+            for word in words:
+                word = word.lower()
+                word = word.rstrip(",. ?")
+                # print(word)
+                counts[word] = counts.get(word, 0) + 1
+
             print(word)
-            counts[word] = counts.get(word, 0) + 1
+            print(words)
 
-    return counts
+        for count, num in counts.items():
+            print(count, ": ", num)
 
-print(words_count(file_name))
+        return counts
+
+        
+
+words_count(sys.argv[1])
